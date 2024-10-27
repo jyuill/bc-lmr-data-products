7bce8a1f11f822db36fe49316da4ae74ba2a68f4
John Yuill <john@fig4.com>
2024-10-27 13:54:30 -0700

    framing up additional tabs with nice help from copilot

--- lmr-data/ui.R
+++ lmr-data/ui.R
@@ -37,65 +37,280 @@ fluidPage(
       # sidebar ----
         sidebarPanel(
           class = "sidebar",
-            # select one or more years, including multiple years
-            # different options tried
-            # selector is good but takes up space, not so intuitive/elegant
-            # selectizeInput(inputId="cyr_select", "Select a year", 
-            #                choices = unique(lmr_data$cyr), 
-            #                selected = unique(lmr_data$cyr),
-            #                multiple = TRUE
-            #                ),
-            # checkbox works but screen real estate
-            # checkboxGroupInput(inputId = "cyr_check", "Select a year", 
-            #                    choices = unique(lmr_data$cyr), 
-            #                    selected = unique(lmr_data$cyr),
-            #                    inline = FALSE
-            #                    ),
-            # picker for max flexibility/usability
-            uiOutput("dynamic_cyr"),
-            # filter for quarters
-            uiOutput("dynamic_qtr"),
-            # filter for categories
-            uiOutput("dynamic_cat"),
+            # conditional panel Tab 1
+            conditionalPanel(
+              condition = "input.tabselected == 1",
+              # select one or more years, including multiple years
+              # different options tried
+              # selector is good but takes up space, not so intuitive/elegant
+              # selectizeInput(inputId="cyr_select", "Select a year", 
+              #                choices = unique(lmr_data$cyr), 
+              #                selected = unique(lmr_data$cyr),
+              #                multiple = TRUE
+              #                ),
+              # checkbox works but screen real estate
+              # checkboxGroupInput(inputId = "cyr_check", "Select a year", 
+              #                    choices = unique(lmr_data$cyr), 
+              #                    selected = unique(lmr_data$cyr),
+              #                    inline = FALSE
+              #                    ),
+              # picker for max flexibility/usability
+              uiOutput("dynamic_cyr"),
+              # filter for quarters
+              uiOutput("dynamic_qtr"),
+              # filter for categories
+              uiOutput("dynamic_cat"),
+            ), # end conditionalPanel Tab 1
+             # conditional panel Tab 2
+            conditionalPanel(
+              condition = "input.tabselected == 2"
+              #,
+              # picker for max flexibility/usability
+              #uiOutput("dynamic_cyr"),
+              # filter for quarters
+              #uiOutput("dynamic_qtr"),
+              # filter for categories
+              #uiOutput("dynamic_cat") 
+            ) # end conditionalPanel Tab 2
         ), # end sidebarPanel
 
         # main panel with content ----
         mainPanel(
           class = "main",
-          fluidRow(
-            tags$h2("Total Sales by Year and Quarter (all categories)", class='section'),
-            column(width = 6,
-                   plotlyOutput("sales_yr")
-            ),
-            column(width = 6,
-                   plotlyOutput("sales_qtr")
-            )
-          ), # end fluidRow 1
-          fluidRow(
-            column(width = 6,
-                   plotlyOutput("sales_yoy", height = "200px")
-            ),
-            column(width = 6,
-                   plotlyOutput("sales_qoq", height = "200px")
-          )
-          ), # end fluidRow 2
-          fluidRow(
-            tags$h2("Category Sales by Year and Quarter", class='section'),
-            column(width = 6,
-                   plotlyOutput("sales_yr_cat")
-            ),
-            column(width = 6,
-                   plotlyOutput("sales_qtr_cat")
-          )
-          ), # end fluidRow 3
-          fluidRow(
-            column(width = 6,
-                   plotlyOutput("sales_yoy_cat", height = "500px")
-            ),
-            column(width = 6,
-                   plotlyOutput("sales_qoq_cat", height = "500px")
-          )
-          ), # end fluidRow 4
+          tabsetPanel( # tabsetPanel ----
+            id = "tabselected",
+            # tabPanel 1: Overview ----
+            tabPanel("Overview", value = 1,
+                     fluidRow( ## fluidRow 1 ----
+                       tags$h2("Total Sales by Year and Quarter (all categories)", class='section'),
+                       column(width = 6,
+                              plotlyOutput("sales_yr")
+                       ),
+                       column(width = 6,
+                              plotlyOutput("sales_qtr")
+                       )
+                     ), # end fluidRow 1
+                     fluidRow( ## fluidRow 2 ----
+                       column(width = 6,
+                              plotlyOutput("sales_yoy", height = "200px")
+                       ),
+                       column(width = 6,
+                              plotlyOutput("sales_qoq", height = "200px")
+                       )
+                    ),
+                     fluidRow( ## fluidRow 3 ----
+                       tags$h2("Category Sales by Year and Quarter", class='section'),
+                       column(width = 6,
+                              plotlyOutput("sales_yr_cat")
+                       ),
+                       column(width = 6,
+                              plotlyOutput("sales_qtr_cat")
+                       )
+                     ), # end fluidRow 3
+                     fluidRow( ## fluidRow 4 ----
+                       column(width = 6,
+                              plotlyOutput("sales_yoy_cat", height = "500px")
+                       ),
+                       column(width = 6,
+                              plotlyOutput("sales_qoq_cat", height = "500px")
+                       )
+                     ), # end fluidRow 4
+            ), # end tabPanel 1
+            # tabPanel 2: Beer ----
+            tabPanel("Beer", value = 2,
+                     fluidRow( ## fluidRow 1 ----
+                       tags$h2("Beer Sales by Year and Quarter (all categories)", class='section'),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yr")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qtr")
+                       )
+                     ), # end fluidRow 1
+                     fluidRow( ## fluidRow 2 ----
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yoy", height = "200px")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qoq", height = "200px")
+                        )
+                     ), # end fluidRow 2
+                     fluidRow( ## fluidRow 3 ----
+                       tags$h2("Beer Category Sales by Year and Quarter", class='section'),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yr_cat")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qtr_cat")
+                       )
+                     ), # end fluidRow 3
+                     fluidRow( ## fluidRow 4 ----
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yoy_cat", height = "500px")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qoq_cat", height = "500px")
+                       )
+                     ) # end fluidRow 4
+            ), # end tabPanel 2
+            # tabPanel 3: Refresh Bev ----
+            tabPanel("Refresh Bev", value = 3,
+                     fluidRow( ## fluidRow 1 ----
+                        tags$h2("COMING SOON", class='section'),
+                       tags$h2("Refreshment Beverage Sales by Year and Quarter (all categories)", class='section'),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yr")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qtr")
+                       )
+                     ), # end fluidRow 1
+                     fluidRow( ## fluidRow 2 ----
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yoy", height = "200px")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qoq", height = "200px")
+                       )
+                     ), # end fluidRow 2
+                     fluidRow( ## fluidRow 3 ----
+                       tags$h2("Refreshment Beverage Category Sales by Year and Quarter", class='section'),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yr_cat")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qtr_cat")
+                       )
+                     ), # end fluidRow 3
+                     fluidRow( ## fluidRow 4 ----
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yoy_cat", height = "500px")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qoq_cat", height = "500px")
+                       )
+                     ) # end fluidRow 4
+            ), # end tabPanel 3
+            # tabPanel 4: Spirits ----
+            tabPanel("Spirits", value = 4,
+                     fluidRow( ## fluidRow 1 ----
+                        tags$h2("COMING SOON", class='section'),
+                       tags$h2("Spirits Sales by Year and Quarter (all categories)", class='section'),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yr")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qtr")
+                       )
+                     ), # end fluidRow 1
+                     fluidRow( ## fluidRow 2 ----
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yoy", height = "200px")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qoq", height = "200px")
+                       )
+                     ), # end fluidRow 2
+                     fluidRow( ## fluidRow 3 ----
+                       tags$h2("Spirits Category Sales by Year and Quarter", class='section'),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yr_cat")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qtr_cat")
+                       )
+                     ), # end fluidRow 3
+                     fluidRow( ## fluidRow 4 ----
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yoy_cat", height = "500px")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qoq_cat", height = "500px")
+                       )
+                     ) # end fluidRow 4
+            ), # end tabPanel 4
+            # tabPanel 5: Wine ----
+            tabPanel("Wine", value = 5,
+                     fluidRow( ## fluidRow 1 ----
+                      tags$h2("COMING SOON", class='section'),
+                       tags$h2("Wine Sales by Year and Quarter (all categories)", class='section'),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yr")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qtr")
+                       )
+                     ), # end fluidRow 1
+                     fluidRow( ## fluidRow 2 ----
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yoy", height = "200px")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qoq", height = "200px")
+                       )
+                     ), # end fluidRow 2
+                     fluidRow( ## fluidRow 3 ----
+                       tags$h2("Wine Category Sales by Year and Quarter", class='section'),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yr_cat")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qtr_cat")
+                       )
+                     ), # end fluidRow 3
+                     fluidRow( ## fluidRow 4 ----
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_yoy_cat", height = "500px")
+                       ),
+                       column(width = 6
+                              #,
+                              #plotlyOutput("sales_qoq_cat", height = "500px")
+                       )
+                     ) # end fluidRow 4
+            ), # end tabPanel 5
+            # tabPanel 6: About ----
+            tabPanel("About", value = 6,
+                     fluidRow( ## fluidRow 1 ----
+                       tags$h2("About the BC Liquor Market Report (LMR) Data", class='section'),
+                       tags$p("This is a Shiny web application that provides an overview of the BC Liquor Market Report (LMR) data."),
+                       tags$p("The data is sourced from the BC Liquor Distribution Branch (LDB) and includes sales data for various categories of alcoholic beverages."),
+                       tags$p("The data is updated quarterly and includes sales data for the current fiscal year and previous fiscal years."),
+                       tags$p("The data is available for the following categories: Beer, Refreshment Beverages, Spirits, and Wine."),
+                       tags$p("The data is available for the following years: 2020, 2021, 2022, 2023, 2024.")
+                     ) # end fluidRow 1
+              ) # end tabPanel 6
+            ) # end tabsetPanel ----
         ) # end mainPanel ----
     ) # end sidebarLayout ----
 ) # end shinyUI
